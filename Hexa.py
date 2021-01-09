8b703867ec0f036fe44638e8f5c395384800d495
Light-Lens <73633374+Light-Lens@users.noreply.github.com>
2021-01-09 12:43:10 +0530

    Initial commit

---
+++ Hexa.py
@@ -0,0 +1,14 @@
+# Hexa programming language
+# Modules are imported that will be used in Hexa programming language.
+import random
+import math
+import sys
+import os
+
+# This import blocks pygame from printing it's startup text.
+from os import environ
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+import pygame
+from pygame.locals import *
+
+pygame.init()
