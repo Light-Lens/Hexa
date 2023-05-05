c36eadead067207d69e40b2e24f0029e9a0d390d
Light-Lens <QCoreNest@gmail.com>
2023-05-06 00:26:13 +0530

    Add input

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 
+using namespace std;
+
 int main(int argv, char** args)
 {
     SDL_Init(SDL_INIT_EVERYTHING);
@@ -10,6 +12,9 @@ int main(int argv, char** args)
 
     bool isRunning = true;
     SDL_Event event;
+
+    int x = 50, y = 50;
+    int xvel = 0, yvel = 0;
 
     while (isRunning)
     {
@@ -24,11 +29,59 @@ int main(int argv, char** args)
                 case SDL_KEYDOWN:
                     if (event.key.keysym.sym == SDLK_ESCAPE)
                         isRunning = false;
+
+                    if (event.key.keysym.sym == SDLK_LEFT)
+                        xvel--;
+
+                    if (event.key.keysym.sym == SDLK_RIGHT)
+                        xvel++;
+
+                    if (event.key.keysym.sym == SDLK_UP)
+                        yvel--;
+
+                    if (event.key.keysym.sym == SDLK_DOWN)
+                        yvel++;
+
+                    break;
+
+                case SDL_KEYUP:
+                    if (event.key.keysym.sym == SDLK_LEFT)
+                    {
+                        if (xvel < 0)
+                            xvel = 0;
+                    }
+
+                    if (event.key.keysym.sym == SDLK_RIGHT)
+                    {
+                        if (xvel > 0)
+                            xvel = 0;
+                    }
+
+                    if (event.key.keysym.sym == SDLK_UP)
+                    {
+                        if (yvel < 0)
+                            yvel = 0;
+                    }
+
+                    if (event.key.keysym.sym == SDLK_DOWN)
+                    {
+                        if (yvel > 0)
+                            yvel = 0;
+                    }
+
+                    break;
             }
         }
 
-        SDL_RenderClear(renderer);
+        x += xvel;
+        y += yvel;
+
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_Rect rect = { x, y, 50, 50 };
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderFillRect(renderer, &rect);
 
         SDL_RenderPresent(renderer);
     }
