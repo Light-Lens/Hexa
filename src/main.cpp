2d688d09d53a20f90f177f0aac98d1c945f28570
Light-Lens <QCoreNest@gmail.com>
2023-05-08 20:57:00 +0530

    Improve the code

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,20 @@ int main(int argv, char** args)
     SDL_Window *window = SDL_CreateWindow("Hexa engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, 0);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
 
+    SDL_Rect square = { 1280/2 - 25, 720/2 - 25, 50, 50 };
+    int xvel = 0, yvel = 0;
+
     bool isRunning = true;
     SDL_Event event;
 
-    int x = 50, y = 50;
-    int xvel = 0, yvel = 0;
-
+    Uint32 lastTick = SDL_GetTicks();
     while (isRunning)
     {
+        // Calculate the time elapsed since the last frame
+        Uint32 currentTick = SDL_GetTicks();
+        float deltaTime = (currentTick - lastTick) / 1000.0f;
+        lastTick = currentTick;
+
         while (SDL_PollEvent(&event))
         {
             switch (event.type)
@@ -27,65 +33,66 @@ int main(int argv, char** args)
                     break;
 
                 case SDL_KEYDOWN:
-                    if (event.key.keysym.sym == SDLK_ESCAPE)
-                        isRunning = false;
+                    switch (event.key.keysym.sym)
+                    {
+                        case SDLK_UP:
+                            yvel = -1000;
+                            break;
 
-                    if (event.key.keysym.sym == SDLK_LEFT)
-                        xvel--;
+                        case SDLK_DOWN:
+                            yvel = 1000;
+                            break;
 
-                    if (event.key.keysym.sym == SDLK_RIGHT)
-                        xvel++;
+                        case SDLK_LEFT:
+                            xvel = -1000;
+                            break;
 
-                    if (event.key.keysym.sym == SDLK_UP)
-                        yvel--;
-
-                    if (event.key.keysym.sym == SDLK_DOWN)
-                        yvel++;
-
+                        case SDLK_RIGHT:
+                            xvel = 1000;
+                            break;
+                    }
                     break;
 
                 case SDL_KEYUP:
-                    if (event.key.keysym.sym == SDLK_LEFT)
+                    switch (event.key.keysym.sym)
                     {
-                        if (xvel < 0)
-                            xvel = 0;
-                    }
-
-                    if (event.key.keysym.sym == SDLK_RIGHT)
-                    {
-                        if (xvel > 0)
-                            xvel = 0;
-                    }
-
-                    if (event.key.keysym.sym == SDLK_UP)
-                    {
-                        if (yvel < 0)
+                        case SDLK_UP:
+                        case SDLK_DOWN:
                             yvel = 0;
-                    }
+                            break;
 
-                    if (event.key.keysym.sym == SDLK_DOWN)
-                    {
-                        if (yvel > 0)
-                            yvel = 0;
+                        case SDLK_LEFT:
+                        case SDLK_RIGHT:
+                            xvel = 0;
+                            break;
                     }
-
                     break;
             }
         }
 
-        x += xvel;
-        y += yvel;
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        // Update the square's position based on its velocity
+        square.x += xvel * deltaTime;
+        square.y += yvel * deltaTime;
+
+        // Clamp the square's position to the screen bounds
+        square.x = max(min(square.x, 1280 - 50), 0);
+        square.y = max(min(square.y, 720 - 50), 0);
+
+        // Clear the screen
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 1);
         SDL_RenderClear(renderer);
 
-        SDL_Rect rect = { x, y, 50, 50 };
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        SDL_RenderFillRect(renderer, &rect);
+        // Draw the square
+        SDL_SetRenderDrawColor(renderer, 43, 153, 255, 1);
+        SDL_RenderFillRect(renderer, &square);
 
+        // Update the screen
         SDL_RenderPresent(renderer);
+        SDL_Delay(10); // Wait for 10 milliseconds
     }
 
+    // Cleanup
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
