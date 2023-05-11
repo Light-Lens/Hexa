0569b0444d97e7ae25750cc5c5718cec033f8924
Light-Lens <QCoreNest@gmail.com>
2023-05-11 15:37:35 +0530

    Implement Player input

---
+++ include/Hexa.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+#include <iostream>
+
+namespace Hexa
+{
+    SDL_Renderer *renderer = nullptr;
+    namespace Engine
+    {
+        class Display
+        {
+        public:
+            int SCREEN_WIDTH;
+            int SCREEN_HEIGHT;
+            std::string SCREEN_TITLE;
+
+        private:
+            SDL_Window *window = nullptr;
+            SDL_Event event;
+
+        public:
+            Display(std::string title="Hexa engine", int width=1280, int height=720);
+
+            void WindowEvent(bool& isrunning);
+            void Update(Uint32 DelayTime=10);
+            void Clear(Uint8 r=255, Uint8 g=255, Uint8 b=255, Uint8 a=1);
+
+            ~Display();
+        };
+
+        class Time
+        {
+        private:
+            Uint32 DeltaTimeLastTick;
+            Uint32 FPSLastTick;
+            int Frames;
+
+        public:
+            Time();
+
+            float DeltaTime();
+            int GetFPS();
+        };
+
+        class Input
+        {
+        private:
+            enum PlayerInputKeyCode
+            {
+                UP,
+                DOWN,
+                LEFT,
+                RIGHT,
+                X,
+                Y
+            };
+
+        public:
+            PlayerInputKeyCode PlayerInput();
+        };
+    }
+
+    namespace Entity
+    {
+        class Square
+        {
+        private:
+            struct DimensionMatrix
+            {
+                int x, y;
+                int w, h;
+            };
+
+            struct ColorMatrix
+            {
+                int r, g, b;
+                int a;
+            };
+
+        public:
+            int x, y;
+            int w, h;
+            SDL_Color Color;
+
+        public:
+            Square(DimensionMatrix dim_matrix, ColorMatrix color_matrix);
+            void draw();
+        };
+    }
+}
